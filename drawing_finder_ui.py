b733e12278484746029025cc0839d95038747759
Leon Knight <leon.knight@flann.com>
2024-10-23 14:00:31 +0100

    Added top level pages

--- drawing_finder_ui.py
+++ drawing_finder_ui.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 from tkinter import messagebox as mb
+from tkinter import ttk
 import os
 import sys
 import subprocess
+import math
 
 drawingPath = '\\\\Filesrv\\Drawings\\PROD\\'
 enquiryPath = '\\\\Filesrv\\CustomerEnquiries\\'
@@ -23,7 +25,9 @@ class MainApplication(tk.Tk):
         self.drawingNumber = tk.StringVar()
         self.drawingNumberHistory = ['empty']*5
         self.enquiryNumber = tk.StringVar()
-
+        
+        self.sub_app_waveguide = None
+        self.sub_app_vswr = None
         self.transparencyBool = tk.IntVar()
         
         self.menuBar = tk.Menu(self)
@@ -41,59 +45,59 @@ class MainApplication(tk.Tk):
         self.rfMenu = tk.Menu(self.menuBar, tearoff=False)
         self.menuBar.add_cascade(label='RF Tools', menu=self.rfMenu)
         self.rfMenu.add_checkbutton(label='Transparency', variable=self.transparencyBool, onvalue=1, offvalue=0, command=lambda: self.transparency_menu())
-        self.rfMenu.add_command(label='Waveguide calculator', command=lambda: self.coming_soon())
-        self.rfMenu.add_command(label='VSWR calculator', command=lambda: self.coming_soon())
+        self.rfMenu.add_command(label='Waveguide calculator', command=lambda: self.open_waveguide_calculator())
+        self.rfMenu.add_command(label='VSWR calculator', command=lambda: self.open_vswr_calculator())
 
         self.helpMenu = tk.Menu(self.menuBar, tearoff=False)
         self.menuBar.add_cascade(label="Help", menu=self.helpMenu)
         self.helpMenu.add_command(label='Help', command=lambda: self.help_menu())
         self.helpMenu.add_command(label='About', command=lambda: self.about_menu())
 
-        self.introTextDrawing = tk.Label(text="Enter drawing number:")
+        self.introTextDrawing = tk.Label(self, text="Enter drawing number:")
         self.introTextDrawing.grid(row=0, column=0, columnspan=1, padx=10, pady=5)
 
-        self.expressionFieldDrawing = tk.Entry(textvariable=self.drawingNumber)
+        self.expressionFieldDrawing = tk.Entry(self, textvariable=self.drawingNumber)
         self.expressionFieldDrawing.grid(row=0, column=1, columnspan=6, padx=0, pady=5)
 
-        self.openButtonDrawing = tk.Button(text='Open', command=lambda: [self.open_drawing(self.drawingNumber.get()), 
+        self.openButtonDrawing = tk.Button(self, text='Open', command=lambda: [self.open_drawing(self.drawingNumber.get()), 
                                                                   self.clear_text_entry()])
         self.openButtonDrawing.grid(row=0,column=7, padx=0, pady=5)
 
-        self.closeButtonDrawing = tk.Button(text='Close All', command=lambda: [subprocess.call('taskkill /f /im InventorView.exe', creationflags=subprocess.CREATE_NO_WINDOW), 
+        self.closeButtonDrawing = tk.Button(self, text='Close All', command=lambda: [subprocess.call('taskkill /f /im InventorView.exe', creationflags=subprocess.CREATE_NO_WINDOW), 
                                                                         subprocess.call('taskkill /f /im dwgviewr.exe', creationflags=subprocess.CREATE_NO_WINDOW), 
                                                                         self.clear_message_box()])
         self.closeButtonDrawing.grid(row=0,column=8, padx=0, pady=5)
 
-        self.introTextEnquiries = tk.Label(text="Enter enquiry number:")
+        self.introTextEnquiries = tk.Label(self, text="Enter enquiry number:")
         self.introTextEnquiries.grid(row=1, column=0, columnspan=1, padx=0, pady=0)
 
-        self.expressionFieldEnquiries = tk.Entry(textvariable=self.enquiryNumber)
+        self.expressionFieldEnquiries = tk.Entry(self, textvariable=self.enquiryNumber)
         self.expressionFieldEnquiries.grid(row=1, column=1, columnspan=6, padx=0, pady=0)
 
-        self.openButtonEnquries = tk.Button(text='Open', command=lambda: [self.open_enquiry(self.enquiryNumber.get()), 
+        self.openButtonEnquries = tk.Button(self, text='Open', command=lambda: [self.open_enquiry(self.enquiryNumber.get()), 
                                                                   self.clear_text_entry()])
         self.openButtonEnquries.grid(row=1,column=7, padx=0, pady=0)
 
         self.openFolderBool = tk.IntVar()
-        self.openFolderCheckbox = tk.Checkbutton(text='Open Folder', variable=self.openFolderBool, onvalue=1, offvalue=0)
+        self.openFolderCheckbox = tk.Checkbutton(self, text='Open Folder', variable=self.openFolderBool, onvalue=1, offvalue=0)
         self.openFolderCheckbox.grid(row=2,column=8, columnspan=2, padx=0, pady=5)
 
         self.openDrawingABool = tk.IntVar(value=True)
         self.openDrawingCBool = tk.IntVar()
         self.openDrawingRBool = tk.IntVar()
 
-        self.openDrawingTypeText = tk.Label(text="Open Drawing Type:")
+        self.openDrawingTypeText = tk.Label(self, text="Open Drawing Type:")
         self.openDrawingTypeText.grid(row=2,column=0, padx=10)
 
-        self.openDrawingACheckbox = tk.Checkbutton(text="A", variable=self.openDrawingABool, onvalue=1, offvalue=0)
+        self.openDrawingACheckbox = tk.Checkbutton(self, text="A", variable=self.openDrawingABool, onvalue=1, offvalue=0)
         self.openDrawingACheckbox.grid(row=2,column=1, columnspan=1, padx=0, pady=5)
-        self.openDrawingCCheckbox = tk.Checkbutton(text="C", variable=self.openDrawingCBool, onvalue=1, offvalue=0)
+        self.openDrawingCCheckbox = tk.Checkbutton(self, text="C", variable=self.openDrawingCBool, onvalue=1, offvalue=0)
         self.openDrawingCCheckbox.grid(row=2,column=2, columnspan=1, padx=0, pady=5)
-        self.openDrawingRCheckbox = tk.Checkbutton(text="R", variable=self.openDrawingRBool, onvalue=1, offvalue=0)
+        self.openDrawingRCheckbox = tk.Checkbutton(self, text="R", variable=self.openDrawingRBool, onvalue=1, offvalue=0)
         self.openDrawingRCheckbox.grid(row=2,column=3, columnspan=1, padx=0, pady=5)
 
-        self.messageBox = tk.Text(height=2, width=50)
-        self.messageBoxScrollBar = tk.Scrollbar(command=self.messageBox.yview, orient="vertical")
+        self.messageBox = tk.Text(self, height=2, width=50)
+        self.messageBoxScrollBar = tk.Scrollbar(self, command=self.messageBox.yview, orient="vertical")
         self.messageBox.configure(yscrollcommand=self.messageBoxScrollBar.set)
         self.messageBox.grid(row=3, column=0, columnspan=10, padx=10, pady=0)
         self.messageBoxScrollBar.grid(row=3, column=11, sticky='ns')
@@ -127,14 +131,22 @@ class MainApplication(tk.Tk):
                     "Please report any bugs and suggest any ideas to Leon")
 
     def about_menu(self):
-        mb.showinfo('About', "Leon's drawing opener\nVersion: 1.2.1")
+        mb.showinfo('About', "Leon's drawing opener\nVersion: 1.3")
 
     def coming_soon(self):
         mb.showinfo('Message','Feature coming soon')
 
-    # def open_waveguide_calculator(self):
-    #     waveguide_calculator = WaveguideCalculatorPage()
-    #     waveguide_calculator.grab_set()
+    def open_waveguide_calculator(self):
+        if self.sub_app_waveguide is None or not self.sub_app_waveguide.winfo_exists():
+            self.sub_app_waveguide = WaveguideCalculatorPage(self)
+        else:
+            self.sub_app_waveguide.focus()
+
+    def open_vswr_calculator(self):
+        if self.sub_app_vswr is None or not self.sub_app_vswr.winfo_exists():
+            self.sub_app_vswr = UnitCalculatorPage(self)
+        else:
+            self.sub_app_vswr.focus()
 
     def filter_file_names(self, fileList, letterType):
         filteredList = []
@@ -212,17 +224,129 @@ class MainApplication(tk.Tk):
             break
             
 
-# class WaveguideCalculatorPage(tk.Toplevel):
-#     def __init__(self, *args, **kwargs):
-#         super().__init__(*args, **kwargs) 
-#         self.title("Waveguide Calculator")
-#         self.geometry("450x120")
-#         self.resizable(False, False)
-#         self.iconbitmap(resource_path('images\\FlannMicrowave.ico'))
+class WaveguideCalculatorPage(tk.Toplevel):
+    def __init__(self, main_app):
+        self.main_app = main_app
+        super().__init__(main_app)
+        self.title("Waveguide Calculator")
+        self.geometry("450x120")
+        self.resizable(False, False)
+
+        self.attributes("-topmost",True)
+
+        self.iconbitmap(resource_path('FlannMicrowave.ico'))
         
-#         self.label = tk.Label(text="Coming Soon!")
-#         self.label.grid(row=0, column=0)
+        self.label = tk.Label(self, text="Coming Soon!")
+        self.label.grid(row=0, column=0)
       
+
+class UnitCalculatorPage(tk.Toplevel):
+    """
+    ConversionTool is a tkinter-based GUI for performing unit conversions.
+    
+    Supported conversions:
+    - Length: km, m, cm, mm, mi, yd, ft, in
+    - Loss: VSWR, RL, S parameter
+    """
+    def __init__(self, main_app):
+        self.main_app = main_app
+        super().__init__(main_app)
+        self.title("Unit Calculator")
+        self.geometry("350x180")
+        self.resizable(False, False)
+
+        self.attributes("-topmost",True)
+
+        self.iconbitmap(resource_path('FlannMicrowave.ico'))
+
+        self.conversion_fns = {
+            'Length': self.length_conversion,
+            'Loss': self.loss_conversion,
+        }
+
+        """
+        Setup the user interface components.
+        """
+        options = {
+            'Length': ['in', 'mm', 'cm', 'm', 'km', 'mi', 'yd', 'ft'],
+            'Loss': ['VSWR', 'dB', 'S11']
+        }
+
+        self.entries = []
+
+        for i, (name, option_values) in enumerate(options.items()):
+            tk.Label(self, text=f'{name} Conversion').grid(column=i, row=0, sticky='W', padx=10, pady=5)
+
+            from_ = ttk.Combobox(self, values=option_values)
+            from_.grid(column=i, row=1, sticky='W', padx=10, pady=0)
+            from_.current(0)
+
+            to_ = ttk.Combobox(self, values=option_values)
+            to_.grid(column=i, row=2, sticky='W', padx=10, pady=0)
+            to_.current(1)
+
+            entry = tk.Entry(self)
+            entry.grid(column=i, row=3, sticky='W', padx=10, pady=5)
+
+            result = tk.Label(self, text='')
+            result.grid(column=i, row=4, sticky='W', padx=10, pady=5)
+
+            self.entries.append((name, entry, result, from_, to_))
+
+        tk.Button(self, text='Calculate', command=self.calculate).grid(column=0, row=5, sticky='W', padx=10, pady=5)
+
+
+    def length_conversion(self, from_unit, to_unit, value):
+        """
+        Convert a length value from one unit to another.
+        
+        Parameters:
+        from_unit (str): The original unit of the length.
+        to_unit (str): The target unit for the conversion.
+        value (float): The length value to convert.
+        
+        Returns:
+        float: The converted length value.
+        """
+        length_dict = {'km': 1000.0, 'm': 1.0, 'cm': 0.01, 'mm': 0.001, 
+                       'mi': 1609.34, 'yd': 0.9144, 'ft': 0.3048, 'in': 0.0254}
+        return (value * length_dict[from_unit]) / length_dict[to_unit]
+    
+    def loss_conversion(self, from_unit, to_unit, value):
+        if from_unit == 'VSWR':
+            s11 = (value - 1) / (value + 1)
+            if to_unit == 'S11':
+                return s11
+            elif to_unit == 'dB':
+                return 20 * math.log10(s11)
+        elif from_unit == 'dB':
+            s11 = 10 ** (value / 20)
+            if to_unit == 'S11':
+                return s11
+            elif to_unit == 'VSWR':
+                return (1 + s11) / (1 - s11)
+        elif from_unit == 'S11':
+            if to_unit == 'dB':
+                return 20 * math.log10(value)
+            elif to_unit == 'VSWR':
+                return (1 + value) / (1 - value)
+        return value
+    
+    def calculate(self):
+        """
+        Perform the unit conversions based on the current entries and update the result labels.
+        """
+        for name, entry, result, from_, to_ in self.entries:
+            value = entry.get()
+            if not value:  # Skip if the entry is empty
+                continue
+            try:
+                value = float(value)
+                conversion_fn = self.conversion_fns[name]
+                result['text'] = conversion_fn(from_.get(), to_.get(), value)
+            except ValueError:
+                tk.messagebox.showerror('Error', f'Invalid input for {name} conversion')
+            
 
 if __name__ == '__main__':
     app = MainApplication()
